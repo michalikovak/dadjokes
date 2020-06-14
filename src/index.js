04e66b38752718d159161fa99b9b5ca58d5a760f
michalikovak <klaramichalikova@gmail.com>
2020-06-14 20:26:18 +0200

    nefunguje, ale snaha byla

--- src/index.js
+++ src/index.js
@@ -3,22 +3,19 @@ import './style.css';
 import Vue from 'vue';
 import './App.vue';
 
-
-let upLikes = 0;
-let downLikes = 0;
-
-const btnUp = document.querySelector('#btn-up');
-const btnDown = document.querySelector('#btn-down');
-
-btnUp.addEventListener('click', () => {
-  upLikes += 1;
-  const likesUpElm = document.querySelector('#likes-up');
-  likesUpElm.textContent = upLikes;
-});
-
-btnDown.addEventListener('click', () => {
-  downLikes += 1;
-  const likesDownElm = document.querySelector('#likes-down');
-  likesDownElm.textContent = downLikes;
+new Vue({
+  el: '#app',
+  data: {
+    upLikes: 0,
+    downLikes: 0,
+  },
+  methods: {
+    btnUp() {
+      this.upLikes += 1;
+    }
+  },
+  btnDown() {
+    this.downLikes += 1;
+  },
 });
 
