3320697678fd2bb2b3f464647dd2fb90cb2122c9
michalikovak <klaramichalikova@gmail.com>
2020-06-14 22:27:20 +0200

    vetev components :D

---
+++ components/Jokes.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ userJoke }}
+    {{ textJoke }}
+    {{ likesJoke }}
+    {{ dislikesJoke }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Jokes",
+  props: ["userJoke", "textJoke", "likesJoke", "dislikesJoke"],
+};
+</script>
+
+<style></style>script
--- src/index.js
+++ src/index.js
@@ -5,17 +5,5 @@ import './App.vue';
 
 new Vue({
   el: '#app',
-  data: {
-    upLikes: 0,
-    downLikes: 0,
-  },
-  methods: {
-    btnUp() {
-      this.upLikes += 1;
-    }
-  },
-  btnDown() {
-    this.downLikes += 1;
-  },
-});
+})
 
